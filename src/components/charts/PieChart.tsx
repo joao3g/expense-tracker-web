e5959df346834778cd26e1fd31a480d0de1975f3
Joao Gabriel <joao.almeida.gabriel2015@gmail.com>
2026-04-24 13:30:18 -0300

    feat(pieChart): Set 'others' slice on pie chart

--- src/components/charts/PieChart.tsx
+++ src/components/charts/PieChart.tsx
@@ -1,13 +1,31 @@
 import { type DefaultizedPieValueType, type PieValueType } from '@mui/x-charts/models';
 import { PieChart, pieArcLabelClasses } from '@mui/x-charts/PieChart';
 
-
-export default function Main({ data }: { data: PieValueType[] }) {
+export default function Main({ data, collapse }: { data: PieValueType[], collapse?: boolean }) {
     const TOTAL = data.map((item) => item.value).reduce((a, b) => a + b, 0);
-    
+
+    if (collapse) {
+        const others = { 
+            value: data.reduce((acc, item) => {
+                const percent = (item.value / TOTAL) * 100;
+                if (percent <= 10) return acc + item.value;
+                return acc;
+            }, 0),
+            label: "Outros"
+        }
+
+        data = data.filter(item => {
+            const percent = (item.value / TOTAL) * 100;
+            if (percent <= 10) return false;
+            return true;
+        });
+
+        data.push(others);
+    }
+
     const getArcLabel = (params: DefaultizedPieValueType) => {
-        const percent = params.value / TOTAL;
-        return `${(percent * 100).toFixed(0)}%`;
+        const percent = (params.value / TOTAL) * 100;
+        return percent > 5 ? `${percent.toFixed(0)}%` : "";
     };
 
     return (
@@ -17,7 +35,7 @@ export default function Main({ data }: { data: PieValueType[] }) {
             series={[
                 {
                     outerRadius: 100,
-                    data,
+                    data: data,
                     arcLabel: getArcLabel,
                     valueFormatter: (param) => new Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" }).format(param.value)
                 },

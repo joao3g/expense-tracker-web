08087f2e62906b188e094634e33903c5481ef7f8
Joao Gabriel <joao.almeida.gabriel2015@gmail.com>
2026-04-24 13:30:46 -0300

    feat(api): add .env for backend api

--- src/api/services/client.ts
+++ src/api/services/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://expense-tracker-api-oiqo.onrender.com",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
